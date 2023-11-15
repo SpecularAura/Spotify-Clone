6f64b435248f84f7108d9f4e0eb1fa0cfe25dbb9
Aum Kulkarni <aumwinay@gmail.com>
2023-11-15 13:27:45 +0530

    Getting songs from youtube and streaming them

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, send_from_directory, send_file
+import io
+from yt_dlp import YoutubeDL
+from contextlib import redirect_stdout
 app = Flask(__name__, 
             static_url_path='/static', 
             static_folder='frontend/build/static')
@@ -6,5 +9,27 @@ app = Flask(__name__,
 @app.route('/')
 def home():
    return send_from_directory('frontend/build', 'index.html')
+
+@app.route('/stream')
+def stream():
+    youtube_url = "https://www.youtube.com/watch?v=kw4tT7SCmaY"
+    ctx = {
+        "outtmpl": "-",
+        'logtostderr': True,
+        'format': 'mp3/bestaudio/best',
+        'postprocessors': [{  # Extract audio using ffmpeg
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+        }]
+    }
+
+    buffer = io.BytesIO()
+    with redirect_stdout(buffer), YoutubeDL(ctx) as foo:
+        foo.download([youtube_url])
+
+    print(buffer.getbuffer().nbytes)
+    return send_file(buffer, mimetype='audio/mpeg')
+
+
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
